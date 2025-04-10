9db47082d2f3d4a840c189529f939015045bb2aa
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-11 00:03:29 +0300

    ASHE

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/users", (req, res) => {
+  res.send("<h1>Hello, Express.js Server!</h1>");
+});
+
+let port = 2000;
+
+app.listen(port, () => {
+  console.log(`listening start at ${port}`);
+});
+app.get('/about', (req, res) => {
+    res.send("This is about page")
+    console.log("about start")
+})
+app.get('/contact', (req, res) => {
+    res.send("This is contact page")
+    console.log("contact start")
+})
