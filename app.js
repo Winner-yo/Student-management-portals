600a2dc4ba67b6bdb63a905b0d2f0dd58e0d2ccd
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-12 23:29:07 +0300

    new

--- app.js
+++ app.js
@@ -18,3 +18,18 @@ app.get('/contact', (req, res) => {
     res.send("This is contact page")
     console.log("contact start")
 })
+
+const event = new Promise((resolve, reject) => {
+  const name = "John"
+  if (name == "Ashe") {
+    resolve(name);
+  } else {
+    reject("Name is not correct: it's "+name)
+  }
+})
+
+event.then((name) => {
+  console.log(name)
+}).catch((err) => {
+  console.log(err)
+})
