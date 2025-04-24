0af10dfe07de60c5eb2835a7c8245c87b96d8e7f
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-24 23:13:48 +0300

    user-models

--- models/users.models.js
+++ models/users.models.js
@@ -1,1 +1,44 @@
-import express from "express";
+import mongoose from "mongodb";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [30, "Name cannot exceed 30 characters"],
+      minLength: [3, "Name should have more than 3 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Email is not valid"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password should have more than 6 characters"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    department: {
+      type: String,
+      required: [true, "Department is required"],
+      enum: ["CSE", "SE", "COM", "POWER"],
+    },
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.model("USER", UserSchema);
+export default User;
